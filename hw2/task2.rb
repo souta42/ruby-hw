4d5f384068f23202b1c75b0bd774dac18b8aa84b
Serhii Tambovtsev <75913746+souta42@users.noreply.github.com>
2021-07-10 23:10:58 +0300

    Update task2.rb

--- hw2/task2.rb
+++ hw2/task2.rb
@@ -1,37 +1,49 @@
-sand = []
-bags = []
-
-puts "Стоимость киллограмма песка A1: "
-sand[0] = gets.to_i
-puts "Стоимость киллограмма песка A2: "
-sand[1] = gets.to_i
-puts "Стоимость киллограмма песка A3: "
-sand[2] = gets.to_i
-puts "Объём ёмкости B1: "
-bags[0] = gets.to_i
-puts "Объём ёмкости B2: "
-bags[1] = gets.to_i
-puts "Объём ёмкости B3: "
-bags[2] = gets.to_i
-
-for i in sand.each
-  if i >= 100
-    puts "Значение не должно превышать 100!"
-    return 0
+def enterValues txt
+  values = []
+  for i in 0..2
+    puts txt + (i + 1).to_s
+    values[i] = gets.to_i
   end
-end
-for i in bags.each
-  if i >= 100
-    puts "Значение не должно превышать 100!"
-    return 0
-  end
+
+  return values
 end
 
-cost = sand.sort
-weight = bags.sort
+def checkNaturals nums
+  for i in nums.each
+    if i > 100 or i <= 0
+      puts "Значение не должно превышать 100 и должно быть больше 0!"
+      puts "Вы ввели: " + i.to_s
+      return false
+    end
+  end
+
+  return true
+end
 
 def profit(cost, weight)
-  res = cost[0] * weight[0] + cost[1] * weight[1] + cost[2] * weight[2]
-  return res
+  sum = 0
+  for i in 0..2
+    sum += cost[i] * weight[i]
+  end
+  return sum
 end
-puts ("Сотрудники смогут получить: " + profit(cost, weight).to_s)
+  
+
+def goldenSand
+  sand = enterValues "Введите стоимость киллограмма песка A"
+  bags = enterValues "Введите объём ёмкости B"
+
+  if !(checkNaturals sand) or !(checkNaturals bags)
+    return 
+  end
+
+  sand.sort!
+  bags.sort!
+
+  prof = profit(sand, bags)
+
+  puts ("Сотрудники смогут получить: " + prof.to_s)
+
+end
+
+goldenSand
