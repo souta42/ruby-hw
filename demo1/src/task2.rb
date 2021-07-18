b7e82c18bc8fa52172ce7feb689c6413977b0525
Serhii Tambovtsev <75913746+soutajo@users.noreply.github.com>
2021-07-19 01:32:13 +0300

    Delete demo1/src directory

--- demo1/src/task2.rb
+++
@@ -1,8 +0,0 @@
-def contains(envelope_1, envelope_2)
-    if ((envelope_1 ["a"] < envelope_2["c"]) and (envelope_1 ["b"] < envelope_2 ["d"])) || ((envelope_1 ["a"] < envelope_2 ["d"]) and (envelope_1 ["b"] < envelope_2 ["c"]))
-        return 2
-    elsif ((envelope_2 ["c"] < envelope_1 ["a"]) and (envelope_2 ["d"] < envelope_1 ["b"])) || ((envelope_2 ["d"] < envelope_1 ["a"]) and (envelope_2 ["c"] < envelope_1 ["b"]))
-        return 1
-    else return 0
-    end
-end
